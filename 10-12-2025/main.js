f4e95735b7125d3b6512be2fff7d88910636d2fc
unknown <kzaid2918@gmail.com>
2025-12-13 17:43:12 +0500

    first commit

---
+++ 10-12-2025/main.js
@@ -0,0 +1,31 @@
+// var score  = 2.4
+// var mathematics = Math.round(score);
+// console.log("mathematics=>", mathematics);
+
+// var rounded = Math.round(-2.5) //
+// console.log("round=>",rounded)
+
+// --nearest round
+
+// var rounded = Math.round(2.1)
+// var rounded = Math.ceil(2.222222)
+// var rounded = Math.floor(2.9)
+// console.log("round=>",rounded)
+
+// var die = (Math.random() * 80) + 1;
+// var dieWithFloor = Math.floor(die);
+// console.log("die", dieWithFloor);
+
+// 0.9,1,2,3,4,5.9,6.9
+// 1,2,3,4,5,6,7
+// assignment (create a die)
+
+// coersion
+// var age = prompt("enter age ")
+// var reult = parseInt(age) + 1
+// var reult = parseFloat(age) + 1
+// console.log(reult)
+
+// var number = 34;
+// var numberInString = number.toString()
+// console.log("numberInString",numberInString)
