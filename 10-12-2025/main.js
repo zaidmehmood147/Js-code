5ee556b502094207dba5aa0f1739df988da711cf
unknown <kzaid2918@gmail.com>
2025-12-13 18:15:08 +0500

    Updated code

--- 10-12-2025/main.js
+++ 10-12-2025/main.js
@@ -1,20 +1,30 @@
-// var score  = 2.4
+// var score  = 2.6
 // var mathematics = Math.round(score);
 // console.log("mathematics=>", mathematics);
 
-// var rounded = Math.round(-2.5) //
+// var rounded = Math.round(-2.5) 
 // console.log("round=>",rounded)
 
 // --nearest round
 
 // var rounded = Math.round(2.1)
-// var rounded = Math.ceil(2.222222)
+// var rounded = Math.ceil(3.3)
 // var rounded = Math.floor(2.9)
 // console.log("round=>",rounded)
 
 // var die = (Math.random() * 80) + 1;
 // var dieWithFloor = Math.floor(die);
-// console.log("die", dieWithFloor);
+// console.log("die", dieWithFloor);\
+
+// var dice = (Math.random() * 80) + 1;
+// var diceWfloor = Math.floor(dice);
+// console.log(diceWfloor);
+
+
+// var dice = (0.6,1.6,2.6,3.6,4.6,5.6);
+// var result = (Math.floor(dice));
+// console.log(result);
+
 
 // 0.9,1,2,3,4,5.9,6.9
 // 1,2,3,4,5,6,7
@@ -22,8 +32,10 @@
 
 // coersion
 // var age = prompt("enter age ")
-// var reult = parseInt(age) + 1
+// var reult = parseInt(age) + 1 //in old class we use plus or number with prompt to add numbers it is same
 // var reult = parseFloat(age) + 1
+//show the floating number like if we put 22.5 in prompt +1 it will show 23.5
+// but in parseint it rounds the number like 22.5 to 23
 // console.log(reult)
 
 // var number = 34;
