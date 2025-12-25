7439e0ae1d379d5f9465fd2b541a3c3784702c0b
unknown <kzaid2918@gmail.com>
2025-12-25 19:30:43 +0500

    Revision js

---
+++ 24-12-2025/main.js
@@ -0,0 +1,49 @@
+// var uname = prompt("enter your name ");
+
+// var arr = ["Saad", "Bilal", "Asad"];
+// //  var unameInLower = uname.toLowerCase()
+// //  var unameInUppercase = uname.toUpperCase()
+// // for(var i =0; i<arr.length; i++){
+// //     if(unameInUppercase ==arr[i]){
+// //         console.log("milgaya",unameInUppercase)
+// //         break;
+// //     }
+// //     else{
+// //         console.log("sorry nhi mila")
+// //     }
+// //     console.log("uname=>",unameInUppercase)
+// // }
+
+// var firstChar = uname.slice(0, 1);
+// var firstChar = firstChar.toUpperCase();
+// // console.log("uppercase", firstChar);
+
+// //  steps to solve a specific problem
+// var remainingChar = uname.slice(1);
+// var remainingChar = remainingChar.toLowerCase();
+// // console.log("remaininf", remainingChar);
+
+// var fullName = firstChar + remainingChar;
+
+// for (var i = 0; i < arr.length; i++) {
+//   if (fullName == arr[i]) {
+//     console.log("milgaya", fullName);
+//     break;
+//   } else {
+//     console.log("sorry nhi mila");
+// }
+// }
+
+// // var text = "this is World War II in asia 1945"
+//           //        World War II
+        
+
+
+// var number = Math.round(0.1)
+// var number = Math.floor(1.8)
+// console.log("number",number)
+
+// var number = prompt("enter your age")
+
+// var age = parseFloat(number) + 10
+// console.log(age)v
