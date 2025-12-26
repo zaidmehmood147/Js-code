b2bcbe50750bfa7f457f41a7a5a29f39b7d28801
unknown <kzaid2918@gmail.com>
2025-12-26 10:29:43 +0500

    while and do while loop

---
+++ 26-12-2025/main.js
@@ -0,0 +1,100 @@
+// function myfuncation(test , lname){
+// console.log("Function called" , test , lname)
+// }
+// myfuncation("zaid", "mehmood") //Arguement
+
+
+// function myfuncation(test , lname){
+// // console.log("Function called" , testlname)
+// var result = test+lname; // if we want to see it out of function
+// return test+lname // if we remove return it will show undefined
+
+// }
+// console.log(myfuncation(2,4)) //Arguement
+
+// function myName() {
+//     var uname = "zaid";
+//     console.log(uname) // keywords are function scope cant run outside function
+// }
+
+// var uname = "zaid";
+// function name() {
+//     uname = "mehmood";
+//     console.log(uname);
+
+// }
+// console.log(uname);
+
+// var uname = "zaid";
+// function func() {
+//     uname = "in function";
+// }
+// uname = "mehmood" //answer is in function
+// func();
+// uname = "mehmood"; // answer is lname
+// console.log(uname)
+
+// Switch statement
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// var age = prompt("Enter your age")
+// switch(age){
+// case 10: // default is optional
+// console.log("age is correct" , age)
+// break
+// case 20:
+// console.log("age is almost correct" , age)
+// break
+// case 30:
+// console.log("age is incorrect" , age)
+// break
+// default: // else
+// console.log("wrong age")
+// }
+
+// var age = prompt("Enter your age")
+// switch(age){
+// case 10: // default is optional
+// console.log("age is correct" , age)
+// break
+// case 20:
+// console.log("age is almost correct" , age)
+// break
+// case 30:
+// console.log("age is incorrect" , age)
+// break
+// default: // else
+// console.log("wrong age")
+// }
+
+//  (dayOfWk ==="Sat" || dayOfWk === "Sun") {
+//  alert("Whoopee!");
+//  }
+//   if (dayOfWk === "Fri") {
+//  alert("TGIF!");
+//  }
+//  else {
+//  alert("Shoot me now!");
+//  }
+
+
+
+// WHILE LOOP
+// var i = 1;
+// while (i<5){
+//    console.log("chal gya") ;
+// }
+
+// .length se koi realtion nhi ha
+
+
+
+//  ask chatgpt for loop and while looooop and do while
+// first do then while
