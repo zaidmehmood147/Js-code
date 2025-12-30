f701a2f216b871a2fe74e158036b4d4fc15f4c0c
unknown <kzaid2918@gmail.com>
2025-12-30 23:34:08 +0500

    Updated code

--- 29-12-2025/main.js
+++ 29-12-2025/main.js
@@ -10,10 +10,8 @@
 // function showPara(paraId){
 // var p = document.getElementById(paraId).innerText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ut nam laudantium dolore maxime. Quaerat soluta totam excepturi veniam impedit inventore corporis enim sunt, aut temporibus asperiores! Neque corrupti architecto dicta fugiat recusandae vel nihil quaerat veritatis numquam odit consequatur totam tenetur accusamus voluptatum a animi illo enim laboriosam temporibus sed, molestias soluta delectus officiis iure. Delectus quae inventore dignissimos adipisci aperiam quaerat labore odio eos quidem quos! Vero perferendis laboriosam atque debitis. Velit ea nemo optio quos amet perspiciatis et omnis impedit! Molestiae nostrum, quasi aperiam facilis nobis animi, alias maxime omnis autem quibusdam consequuntur quis sint provident iste"
 // }
-
-var p = document.getElementById('inputField').style.color="white"
-console.log("p=>",p)
-
-function redField(){
-    
-}
+//zaid
+// var p = document.getElementById('inputField').style.color="white"
+// console.log("p=>",p)
+// function redField(){
+// }
