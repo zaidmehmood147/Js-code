35dde223e360a55607ca0dd5fedc8a7ed4fc6cbd
unknown <kzaid2918@gmail.com>
2026-01-01 22:15:33 +0500

    Updated Events

--- 29-12-2025/main.js
+++ 29-12-2025/main.js
@@ -3,15 +3,27 @@
 // it gives output
 // if we have () it can be function or method
 
+// how to connect external js w html?
+// document calls the html file 
+// if the html element has id or class 
+// for example the p has id(para) to call it in js we will use document.getElementbyId(para)
+document.getElementById("zaid"  )//.innerText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, aspernatur culpa possimus repudiandae "
 
-// function test(msg) {
-//     alert(message);
-// }
-// function showPara(paraId){
-// var p = document.getElementById(paraId).innerText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ut nam laudantium dolore maxime. Quaerat soluta totam excepturi veniam impedit inventore corporis enim sunt, aut temporibus asperiores! Neque corrupti architecto dicta fugiat recusandae vel nihil quaerat veritatis numquam odit consequatur totam tenetur accusamus voluptatum a animi illo enim laboriosam temporibus sed, molestias soluta delectus officiis iure. Delectus quae inventore dignissimos adipisci aperiam quaerat labore odio eos quidem quos! Vero perferendis laboriosam atque debitis. Velit ea nemo optio quos amet perspiciatis et omnis impedit! Molestiae nostrum, quasi aperiam facilis nobis animi, alias maxime omnis autem quibusdam consequuntur quis sint provident iste"
-// }
-//zaid
-// var p = document.getElementById('inputField').style.color="white"
-// console.log("p=>",p)
-// function redField(){
-// }
+function fullparagraph(){
+ var p = document.getElementById(zaid).innerText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ut nam laudantium dolore maxime. Quaerat soluta totam excepturi veniam impedit inventore corporis enim sunt, aut temporibus asperiores! Neque corrupti architecto dicta fugiat recusandae vel nihil quaerat veritatis numquam odit consequatur totam tenetur accusamus voluptatum a animi illo enim laboriosam temporibus sed, molestias soluta delectus officiis iure. Delectus quae inventore dignissimos adipisci aperiam quaerat labore odio eos quidem quos! Vero perferendis laboriosam atque debitis. Velit ea nemo optio quos amet perspiciatis et omnis impedit! Molestiae nostrum, quasi aperiam facilis nobis animi, alias maxime omnis autem quibusdam consequuntur quis sint provident iste"
+
+}
+
+
+
+function test(msg) {
+    alert(message);
+}
+function showPara(paraId){
+var p = document.getElementById(paraId).innerText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ut nam laudantium dolore maxime. Quaerat soluta totam excepturi veniam impedit inventore corporis enim sunt, aut temporibus asperiores! Neque corrupti architecto dicta fugiat recusandae vel nihil quaerat veritatis numquam odit consequatur totam tenetur accusamus voluptatum a animi illo enim laboriosam temporibus sed, molestias soluta delectus officiis iure. Delectus quae inventore dignissimos adipisci aperiam quaerat labore odio eos quidem quos! Vero perferendis laboriosam atque debitis. Velit ea nemo optio quos amet perspiciatis et omnis impedit! Molestiae nostrum, quasi aperiam facilis nobis animi, alias maxime omnis autem quibusdam consequuntur quis sint provident iste"
+}
+zaid
+var p = document.getElementById('inputField').style.color="white"
+console.log("p=>",p)
+function redField(){
+}
