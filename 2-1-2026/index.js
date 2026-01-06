42ed41734a6565b789213e522d7b39cafc7f4d03
unknown <kzaid2918@gmail.com>
2026-01-06 12:13:45 +0500

    Updated code

--- 2-1-2026/index.js
+++ 2-1-2026/index.js
@@ -62,6 +62,13 @@ function expandPara() {
   console.log("para", para);
 }
 
+function longpara() {
+  var lpara = "Lorem ipsum dolor,ratione?assumenda! Voluptates sint consequatur rerum, laborum distinctio iusto, voluptates corrupti quod autem asperiores?tempora sapiente vitae pariatur, asperiores excepturi labore amet quibusdam Cupiditate molestiae reprehenderit architecto veniam culpa minima aut cumque reprehenderit, eius provident, pariatur nam saepe mollitia atque temporibus placeat consequuntur dolorum quasi nihilsed natus facilis fugiat dolorem deserunt totam illumsit  Iste repellendus voluptate, ad mollitia optio minima maiores itaque labore  provident debitis officiis vitae assumenda autem nulla reprehenderit at! amet consectetur adipisicing elit. reprehenderit quis! Et, veritatis necessitatibus quae cum voluptatibus eiusExercitationemipsa accusantium illum debitis ut fugiat architecto eaque dol"
+  var p = document.getElementById("p").innerText = lpara ;
+  
+    
+}
+
 function renderList(){
     var orderlist = document.getElementById("list");
     var lists = "<img src='https://plus.unsplash.com/premium_photo-1661963826911-f369fa24c1a6?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YmlrZXN8ZW58MHx8MHx8fDA%3D' alt='' width='300px'>"
