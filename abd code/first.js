d21a5785f4b2046993a33f2e4edd9bd855096409
unknown <kzaid2918@gmail.com>
2026-01-12 18:57:04 +0500

    Updated Code

---
+++ abd code/first.js
@@ -0,0 +1,38 @@
+// console.dir(document.body)
+// document.getElementById("heading")
+// console.dir(heading);
+// var headings = document.getElementsByClassName("myclass");
+// console.dir(headings);
+// console.log(headings);
+//  var firstEL = document.querySelector("p");//firstEL use code
+// console.dir(firstEL);
+// var allEl = document.querySelectorAll("p");//allEL use code
+// console.dir(allEl);
+//  var firstclass = document.querySelector(".myclass");
+// console.dir(firstclass);
+//  var firstid = document.querySelector("#myid");
+// console.dir(firstid);
+// var div = document.querySelector("div");
+// console.dir(div);
+// var heading = document.querySelector("h1");
+// var h2 = document.querySelector("h2");
+// console.dir(h2);
+// h2.innerText = h2.innerText + "from abdullah";//concatenate
+// var divs = document.querySelectorAll(".box");
+// console.log(divs[0]);
+// divs[0].innerText = "new unique value 1"       
+// divs[1].innerText = "new unique value 2"       
+// divs[2].innerText = "new unique value 3"    
+// var idx = 1;
+// for(div of divs){
+//  div.innerText = new unique value ${idx};
+//  idx++;
+// } 
+// var div = document.querySelector("div");
+// console.log(div);
+// var id = div.getAttribute("id");
+// console.log(id);
+// var name = div.getAttribute("name");
+// console.log(name);
+// var para = document.querySelector("p");
+// console.log(para.getAttribute("class"));
