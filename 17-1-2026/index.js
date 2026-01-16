2fa711519c51afa1282b3aa16f1314479005e7fc
unknown <kzaid2918@gmail.com>
2026-01-16 21:52:41 +0500

    Updated code

--- 17-1-2026/index.js
+++ 17-1-2026/index.js
@@ -4,4 +4,11 @@
 // var studentData = [10, 20, "male","ali"];
 // console.log(studentData[3])
 
-//
+////  --- objects
+//now every value have key(10 have key called roll(value))
+// object (key:value)
+// var studentData = '{ "roll": 10, "age": 20, "gender": "male", "name": "ali" }';
+// frontendData = JSON.parse(studentData)
+// // { "roll": 10, "age": 20, "gender": "male", "name: "ali" }
+// console.log(studentData["name"])
+// console.log(studentData["age"])
