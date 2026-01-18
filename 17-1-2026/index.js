b55b64322dd7de24eb33ef599d639b674e6219dc
unknown <kzaid2918@gmail.com>
2026-01-18 23:01:36 +0500

    Updated code

--- 17-1-2026/index.js
+++ 17-1-2026/index.js
@@ -1,96 +1,96 @@
-// var arr = ["a", "b", 2, true];
-// console.log(arr);
+// // var arr = ["a", "b", 2, true];
+// // console.log(arr);
 
-// var studentData = [10, 20, "male","ali"];
-// console.log(studentData[3])
+// // var studentData = [10, 20, "male","ali"];
+// // console.log(studentData[3])
 
-////  --- objects
-//now every value have key(10 have key called roll(value))
-// object (key:value)
-// var studentData = '{ "roll": 10, "age": 20, "gender": "male", "name": "ali" }';
-// frontendData = JSON.parse(studentData)
-// // { "roll": 10, "age": 20, "gender": "male", "name: "ali" }
-// console.log(studentData["name"])
-// console.log(studentData["age"])
+// ////  --- objects
+// //now every value have key(10 have key called roll(value))
+// // object (key:value)
+// // var studentData = '{ "roll": 10, "age": 20, "gender": "male", "name": "ali" }';
+// // frontendData = JSON.parse(studentData)
+// // // { "roll": 10, "age": 20, "gender": "male", "name: "ali" }
+// // console.log(studentData["name"])
+// // console.log(studentData["age"])
 
-// var student = {
-//   name: "ali",
-//   age: 10,
-//   subjects: ["maths", "science", "english"],
-//   bio: function () {
-//     console.log("hi i am inside object");
-//   },
-//   car: {
-//     name: "corolla",
-//     cc: 760,
-//     fuelAverage: { low: 200, high: [300, 400] },
-//   },
-// //   alert: alert("message from object"),
-// };
+// // var student = {
+// //   name: "ali",
+// //   age: 10,
+// //   subjects: ["maths", "science", "english"],
+// //   bio: function () {
+// //     console.log("hi i am inside object");
+// //   },
+// //   car: {
+// //     name: "corolla",
+// //     cc: 760,
+// //     fuelAverage: { low: 200, high: [300, 400] },
+// //   },
+// // //   alert: alert("message from object"),
+// // };
 
-var countries = {
-  japan : ["tokyo","osaka","hisenberg"] ,
-  usa : ["java" , "css" , "html"],
-  krose : {
-    icon : "momento" ,
-    riffs : "de nuit",
-    initio : {
-      do : "dont" ,
-      was : "were" , 
-      ionic : "nodejs"
-    }
-  } 
-}
-console.log(countries.krose.initio.ionic)
+// var countries = {
+//   japan : ["tokyo","osaka","hisenberg"] ,
+//   usa : ["java" , "css" , "html"],
+//   krose : {
+//     icon : "momento" ,
+//     riffs : "de nuit",
+//     initio : {
+//       do : "dont" ,
+//       was : "were" , 
+//       ionic : "nodejs"
+//     }
+//   } 
+// }
+// console.log(countries.krose.initio.ionic)
 
-// console.log(student.bio);
-// console.log(student["name"])
-// console.log(student.name)
-// console.log(student.subjects[1])
-// console.log(student.bio)
-// console.log(student.car["name"]);
-// console.log(student.car.cc);
+// // console.log(student.bio);
+// // console.log(student["name"])
+// // console.log(student.name)
+// // console.log(student.subjects[1])
+// // console.log(student.bio)
+// // console.log(student.car["name"]);
+// // console.log(student.car.cc);
 
-// var obj = new Object(); //object constructor
+// // var obj = new Object(); //object constructor
+// // var arr = new Array();
+// // arr[100]= "a"
+// // arr[101]="b"
+// // console.log(arr.length)
+// // obj.name = "ibrahim";
+// // obj.age = 20;
+// // obj.subjects= ["maths", "science", "english"]
+// // console.table(obj.subjects[2])
+
+
+// var obj = new Object();
 // var arr = new Array();
-// arr[100]= "a"
-// arr[101]="b"
-// console.log(arr.length)
-// obj.name = "ibrahim";
-// obj.age = 20;
-// obj.subjects= ["maths", "science", "english"]
-// console.table(obj.subjects[2])
+// arr[1010] = "zaid";
+// // console.log(arr.length)
+// obj.zaid = "zt" ; 
+// obj.cocomo = "this is cocomo";
+// console.log(obj.cocomo)
+
+// // var obj = {
+// //   name: "ali",
+// //   age: 30,
+// // };
+// // obj.roll = 10;
+// // obj.bio = undefined;
+// // // console.log(obj);
+// // delete obj.name
+// // console.log("roll" in obj); //boolean true or false
 
 
-var obj = new Object();
-var arr = new Array();
-arr[1010] = "zaid";
-// console.log(arr.length)
-obj.zaid = "zt" ; 
-obj.cocomo = "this is cocomo";
-console.log(obj.cocomo)
 
+// for (let keys in obj) {
+//   console.log("keys=>", keys, "value=>" ,obj[keys]); // shows all the keys and their values
+// }
+// console.log("break")
 // var obj = {
 //   name: "ali",
-//   age: 30,
+//   age: 20,
+//   funct: ()=> {
+//     console.log("value=>", this.name);
+//   },
 // };
-// obj.roll = 10;
-// obj.bio = undefined;
-// // console.log(obj);
-// delete obj.name
-// console.log("roll" in obj); //boolean true or false
-
-
-
-for (let keys in obj) {
-  console.log("keys=>", keys, "value=>" ,obj[keys]); // shows all the keys and their values
-}
-console.log("break")
-var obj = {
-  name: "ali",
-  age: 20,
-  funct: ()=> {
-    console.log("value=>", this.name);
-  },
-};
-obj.funct(); // calling func
+// obj.funct(); // calling func
