7c07293dc7700e7210f5dcae777e7979824d70ae
unknown <kzaid2918@gmail.com>
2026-01-27 15:31:28 +0500

    updated code:

--- 21-1-2026/index.js
+++ 21-1-2026/index.js
@@ -6,30 +6,35 @@
 
 // var address = prompt("enter address");
 // var country = prompt("enter country country");
-const obj3 = {
-  bio: ["ali", "khan", 20],
-  address: { city: "karachi", country: "pakistan", state: "sindh" },
-};
-// console.log(obj3[data][country])
-// console.log(obj3.address.country);
-console.log(obj3.bio["1"])
+// const obj3 = {
+//   bio: ["ali", "khan", 20],
+//   address: { city: "karachi", country: "pakistan", state: "sindh" },
+// };
+// // console.log(obj3[data][country])
+// // console.log(obj3.address.country);
+// console.log(obj3.bio["1"])
 
 // var firstKey = prompt("enter first key");
 // var secondKey = prompt("enter second key");
-// const obj3 = {
-//   bio: ["ali", "khan", 20],
-//   address: {
-//     city: "karachi",
-//     country: "pakistan",
-//     state: { smallState: "shahra e faisal", largeState: "paf" },
-//   },
-// };
+const obj3 = {
+  bio: ["ali", "khan", 20],
+  address: {
+    city: "karachi",
+    country: "pakistan",
+    state: { smallState: "shahra e faisal", largeState: "paf" },
+  },
+};
 // console.log(obj3[firstKey][secondKey]);
+
+// console.log(obj3.address.state.largeState)
+
+// console.log(obj3["address"]["state"]["smallState"])
+
 // console.log(obj3.address.state.largeState);
 // console.log(obj3["address"]["state"]["smallState"]);
 
-// "abc"
-// console.log(abc)
+var abc = "abc"
+console.log(abc)
 
 // var person = {
 //    name:"ali",
