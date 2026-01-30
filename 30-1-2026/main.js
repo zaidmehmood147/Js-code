9837f52485b8027e2c69ee9ca2293f9a6ca53722
unknown <kzaid2918@gmail.com>
2026-01-30 22:10:13 +0500

    updated js

---
+++ 30-1-2026/main.js
@@ -0,0 +1,134 @@
+// console.log("js zinda he")
+
+// var arr =[{name:"ali"},{name:"bilal"}]
+
+// for (let index = 0; index < arr.length; index++) {
+//     console.log(arr[index])
+// }
+
+// var arrMap = arr.map(function(data){
+//     return data
+// })
+// console.log("arrMap",arrMap)
+
+// fetch('https://dummyjson.com/users')
+// .then(res => res.json())
+// .then(console.log);
+
+// console.log(uname)
+// // tdz
+// let uname = "ali"
+
+// if (true) {
+//     let fname = "ali"
+// }
+// console.log("fname=>",fname)
+
+// function test() {
+//     var fname = "ali2"
+// }
+// test()
+// console.log("fname=>",fname)
+token = "fregerfger"
+function outer() {
+    let fname = 12
+    function inner() {
+        let lname = "khan"
+        fname++
+        console.log(fname, lname)
+    }
+    inner()
+    inner()
+    inner()
+}
+outer()
+
+// function test2(){
+//     let a = 2
+//     return a
+// }
+// test2()
+
+
+
+// console.log(a)
+
+// function add(a,b){
+//    return  result = a + b
+// }
+// console.log(result())
+
+
+
+// function add(){
+//     let counter = 20
+//     counter++
+//     return counter
+// }
+// add()
+// add()
+// console.log(counter)
+
+// function add() {
+//   let counter = 0;
+//   function plus() {
+//     counter += 1;
+// console.log(counter)
+// }
+//   plus();   
+// }
+// add()
+// add()
+
+
+
+// function myCounter2() {
+//   let counter = 0;
+//   return function() {
+//     counter++; // 1  ,2,3
+//     console.log(counter)
+//     return counter;
+//   };
+// }
+// const add = myCounter2();
+// add();
+// add();
+
+
+
+
+// class studentData {
+//     constructor(fname, lname) {
+//         this.fname = fname
+//         this.lname = lname
+//     }
+//     getData(){
+//         return console.log(this.fname + this.lname)
+//     }
+   
+// }
+
+// class StudentAge extends  studentData{
+//     // constructor
+// }
+
+
+
+// var student1 = new studentData("ali","khan")
+
+// student1.getData()
+
+
+// synchronous  //line by line
+// asynchronous //
+
+console.log("first line")
+
+async function test() {
+ setTimeout(()=>{
+    console.log("set time out")
+},2000)
+}
+setTimeout(test)
+
+console.log("second line")
