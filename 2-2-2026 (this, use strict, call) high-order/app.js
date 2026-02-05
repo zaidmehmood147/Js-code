c21bc706f73eff5918115242a728e79bdff7d160
unknown <kzaid2918@gmail.com>
2026-02-05 13:42:31 +0500

    updated code 2f

---
+++ 2-2-2026 (this, use strict, call) high-order/app.js
@@ -0,0 +1,30 @@
+// "use strict"
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     fullName: function()  {
+//         return this.firstName + " " + this.lastName;
+//     }
+// };
+
+// console.log(person.fullName())
+// "use strict"
+// let mname = this;
+// console.log(mname)
+
+//  --- call()
+
+// function student(fname) {
+//     return console.log(`hello  ${fname}  ${this.roll}  ` )
+// }
+// const student1 = {
+//     firstName: "bilal",
+//     lastName: "awan",
+//     age: 20,
+//     roll:102
+// }
+// const student2 = {
+//     firstName: "kashif",
+//     lastName: "khan",
+//     age: 30
+// }
