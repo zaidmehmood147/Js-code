893ac6887c404a117753822a797ec34d13e7e588
unknown <kzaid2918@gmail.com>
2026-02-05 13:38:17 +0500

    updated code

---
+++ 4-2-2026 (this, call, apply, bind)/func.js
@@ -0,0 +1,24 @@
+// const mainobj={
+//       stdData:function(message){
+//  console.log(message + this.name)
+// }
+// }
+// const obj = {
+//     name :"ali",
+//     age:20,
+// }
+// mainobj.stdData.call(obj,"hello")
+
+// const mainobj={
+//       stdData:function(message, message2,message3,message4){
+//  console.log(message + message2 + this.funct() + message3 + message4)
+// }
+// }
+// const obj = {
+//     name :"ali",
+//     age:20,
+//     funct :function(){
+//         alert("you are late")
+//     }
+// }
+// mainobj.stdData.apply(obj,["hello  ","hello2 ","hello3 ","hello2 ","hello3 "])
