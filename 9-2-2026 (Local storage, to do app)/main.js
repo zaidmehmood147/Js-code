37497e80c690e9d8757596e773683e4b79089bb1
unknown <kzaid2918@gmail.com>
2026-02-11 14:00:10 +0500

    updated code

--- 9-2-2026 (Local storage, to do app)/main.js
+++ 9-2-2026 (Local storage, to do app)/main.js
@@ -5,14 +5,15 @@
 
 // console.log(window.localStorage);
 
-// // var uname = "ali2";
+// var uname = ["ali2" , "zaid"];
 
-// // localStorage.setItem //give data
-// // localStorage.getItem //take data
-// // localStorage.setItem("name", uname);
-// // var getdata = localStorage.getItem("name");
-// // console.log(getdata);
-// // localStorage.removeItem("object")
+// localStorage.setItem //give data
+// localStorage.getItem //take data
+// localStorage.setItem("name", uname);
+// // var uname = "zaid"
+// var getdata  = localStorage.getItem("name");
+// console.log(getdata);
+// localStorage.removeItem("object")
 // var obj = {
 //   uname: "ali",
 //   age: 20,
@@ -25,37 +26,37 @@
 // // now convert in real object
 // var objConvertInObject = JSON.parse(getObjectInString)//now in obj
 // console.log("objConvertInObject",objConvertInObject.uname)
-// // localStorage.removeItem("object")
+// localStorage.removeItem("object")
 // localStorage.clear()
 
-// -- Todo App With Local storage
-var todoInput = document.getElementById("todoInput");
-var todoBtn = document.getElementById("todoBtn");
-var todoList = document.getElementById("todoList");
-getData();
-todoBtn.addEventListener("click", function () {
-  var todoText = todoInput.value;
-  console.log("todotext=>", todoText);
-  var li = document.createElement("li");
-  li.textContent = todoText;
-  console.log("li=>", li);
-  todoList.appendChild(li);
-  todoInput.value = "";
-  saveData(todoText);
-});
+// // -- Todo App With Local storage
+// var todoInput = document.getElementById("todoInput");
+// var todoBtn = document.getElementById("todoBtn");
+// var todoList = document.getElementById("todoList");
+// getData();
+// todoBtn.addEventListener("click", function () {
+//   var todoText = todoInput.value;
+//   console.log("todotext=>", todoText);
+//   var li = document.createElement("li");
+//   li.textContent = todoText;
+//   console.log("li=>", li);
+//   todoList.appendChild(li);
+//   todoInput.value = "";
+//   saveData(todoText);
+// });
 
-function saveData(todoText) {
-  var todoData = JSON.parse(localStorage.getItem("todo")) || [];
-  todoData.push(todoText);
-  localStorage.setItem("todo", JSON.stringify(todoData));
-}
-function getData() {
-  var gettodo = JSON.parse(localStorage.getItem("todo")) || [];
+// function saveData(todoText) {
+//   var todoData = JSON.parse(localStorage.getItem("todo")) || [];
+//   todoData.push(todoText);
+//   localStorage.setItem("todo", JSON.stringify(todoData));
+// }
+// function getData() {
+//   var gettodo = JSON.parse(localStorage.getItem("todo")) || [];
 
-  gettodo.forEach((todo) => {
-    var li = document.createElement("li");
-    li.textContent = todo;
-    todoList.appendChild(li);
-  });
-}
-getData();
+//   gettodo.forEach((todo) => {
+//     var li = document.createElement("li");
+//     li.textContent = todo;
+//     todoList.appendChild(li);
+//   });
+// }
+// getData();
