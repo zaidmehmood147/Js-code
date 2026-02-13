7fdee5c154b438e4ea461671b59fa5c68245e509
unknown <kzaid2918@gmail.com>
2026-02-13 20:09:41 +0500

    updated code

---
+++ 13-2-2026 ( call back func, arrow func, setTimeout )/index.js
@@ -0,0 +1,22 @@
+// functions 
+// callback functions
+
+// const funct = function myFunc(){
+//     console.log("inside fuction")
+// }
+// funct()// declaration //expression
+
+// function calculate(a,b){
+//    console.log(a+b)
+// }
+// calculate(2,3)
+// es6
+// const arrowFunction = (a,b) => console.log(a+b)
+// arrowFunction(4,5)
+// var num1 = +prompt("enter num1")
+// var num2 = +prompt("enter num2")
+// var obj = {
+//     // a:3,
+//     b:6,
+
+// }
