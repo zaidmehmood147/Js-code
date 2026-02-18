69f2027c5a4597987b2b1f2268f92497d0fae7c8
unknown <kzaid2918@gmail.com>
2026-02-18 16:52:00 +0500

    updated Promises

--- 18-2-2026 Promise/main.js
+++ 18-2-2026 Promise/main.js
@@ -29,3 +29,70 @@
 // myPromise
 // .then(zaid => console.log(zaid))
 // .catch(zed => console.log(zed))
+
+
+// fetchUser(1)
+//     .then(user => {
+//         console.log("Got user:", user.name);
+//         return user.id; // Passing ID to the next step
+//     })
+//     .then(userId => {
+//         return fetchPosts(userId); // Returning a NEW promise
+//     })
+//     .then(posts => {
+//         console.log("Got posts:", posts.length);
+//     })
+//     .catch(error => {
+//         // This ONE catch handles errors from ANY of the steps above
+//         console.error("Global Error Handler:", error);
+//     });
+
+// const getDashboardData = () => {
+//   // Create 3 independent promises
+//   const productsPromise = fetch('https://dummyjson.com/products?limit=3').then(res => res.json());
+//   const usersPromise = fetch('https://dummyjson.com/users?limit=3').then(res => res.json());
+//   const cartsPromise = fetch('https://dummyjson.com/carts?limit=3').then(res => res.json());
+
+//   // Wait for ALL of them to finish
+//   Promise.all([productsPromise, usersPromise, cartsPromise])
+//     .then(([products, users, carts]) => {
+//       console.log("--- Dashboard Loaded ---");
+//       console.log("Products count:", products.products.length);
+//       console.log("Users count:", users.users.length);
+//       console.log("Carts count:", carts.carts.length);
+//     })
+//     .catch(err => {
+//       console.error("One of the requests failed, so they all failed!", err);
+//     });
+// };
+
+// getDashboardData();
+
+// Method,Use Case,Result
+// .then().then(),Sequential tasks (Step A then Step B).,Returns the last value.
+// Promise.all(),Speed! Multiple tasks at once.,Fails if any fail.
+// Promise.allSettled(),Bulk tasks where you don't mind if some fail.,Returns results for all.
+// Promise.race(),"Timeouts or ""First one wins"" scenarios.",Returns the fastest result.
+
+
+// In JavaScript, we use .then() for the "Success" and .catch() for the "Failure."
+
+// Imagine this is the Burger Shop's system
+// const orderBurger = new Promise((resolve, reject) => {
+//     let isBurgerReady = true; // Change this to false to see the error!
+
+//     if (isBurgerReady) {
+//         resolve("🍔 Here is your burger!");
+//     } else {
+//         reject("❌ Sorry, we ran out of ingredients.");
+//     }
+// });
+
+// // Now, YOU (the customer) use the pager:
+// orderBurger
+//     .then((food) => {
+//         console.log(food); // This runs if it resolved
+//     })
+//     .catch((error) => {
+//         console.log(error); // This runs if it rejected
+//     });
