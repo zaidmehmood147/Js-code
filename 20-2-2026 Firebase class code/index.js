fa011a4c9d7c6ee128f964902db636ff4ce7958b
unknown <kzaid2918@gmail.com>
2026-02-21 13:05:40 +0500

    updated code

---
+++ 20-2-2026 Firebase class code/index.js
@@ -0,0 +1,17 @@
+// 1.type module in script
+// 2.live server
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyDKU1D-ZgLrmgTpAbgMgJtA4U4e70jnb8M",
+  authDomain: "smit-mwa-batch-18.firebaseapp.com",
+  projectId: "smit-mwa-batch-18",
+  storageBucket: "smit-mwa-batch-18.firebasestorage.app",
+  messagingSenderId: "514258031376",
+  appId: "1:514258031376:web:7f5d9b6f2209fb72970b95",
+  measurementId: "G-JT7ML11JCK",
+};
