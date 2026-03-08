6eeef71845743bd8cfc46440248f1475008e83ab
Zaid Mehmood <kzaid2918@gmail.com>
2026-03-08 15:12:26 +0500

    Added 6 mar

---
+++ 6-3-2026 realtime firestore/realtime.js
@@ -0,0 +1,17 @@
+
+import { getDatabase, ref, set } from "./firebase.js";
+import { app } from "./index.js";
+const db = getDatabase(app);
+console.log("realtime db=>", db);
+
+var name = document.getElementById("name");
+var message = document.getElementById("message");
+var sendBtn = document.getElementById("sendBtn");
+sendBtn.addEventListener("click", sendMessage);
+async function sendMessage() {
+    const id = new Date();
+    set(ref(db, "messages/" + id), {
+    author: name.value,
+    text: message.value,
+  });
+}
