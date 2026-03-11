71fad406343b0e337fb752e405a76dc907779ca6
Zaid Mehmood <kzaid2918@gmail.com>
2026-03-11 21:56:31 +0500

    updated code

---
+++ 11-3-2026 practice/main.js
@@ -0,0 +1,16 @@
+// a = 4 ;
+// b = 4 ;
+
+
+// if (a===b) {
+//     console.log("a is equals to b")
+// }
+// else {
+//     console.log("error")
+// }
+
+
+// var date = new Date()
+// console.log(date)
+
+
