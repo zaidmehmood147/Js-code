23d643f5e61c70fdd0cbaeee2bb4d8d08c56b39d
Zaid Mehmood <kzaid2918@gmail.com>
2026-03-29 20:18:46 +0500

    added code

--- 29-3-2026 oop/main.js
+++ 29-3-2026 oop/main.js
@@ -1,1 +1,25 @@
 // oop
+
+// befor OOP
+// code hard to read
+// hard to reuseable
+// extend  issues
+// hard to maintain
+
+// es5 (classes - blueprint)
+// class students (name,age,sub,class,id,roll)
+// Object
+// student1 (age,id)
+// OOP = classes + object(properties or data ,method)
+
+
+// classes + object = OOP
+// javascript ->multi paradigm , imperative(;),event-driven
+// paradigm -> way to code
+
+// OOP -> object oriented programming
+// 4 pillars 
+// 1.Abstraction
+// 2.Inherit
+// 3.encapsulation
+// 4.polymorphism
