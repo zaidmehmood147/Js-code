1d7b0519b5866cdee187609ea6ec6e0a73d67a96
Zaid Mehmood <kzaid2918@gmail.com>
2026-03-31 19:21:50 +0500

    updated code

--- 29-3-2026 oop/main.js
+++ 29-3-2026 oop/main.js
@@ -60,45 +60,155 @@
 
 // function area(length){
 //     return 5 * length  //abstraction 
-// }
-// console.log(area(10))
-
-// function pi(radius){
-//     return Math.PI * radius *radius  //abstraction 
 // // }
-// // console.log(area(5))
+// // console.log(area(10))
 
-// // class Car{
-//     constructor(model){
-//         this.model=model
-//     }
-// details(){
-//     console.log("car model is " + this.model)
-//     return this.model
-// }
-// }
-// const mycar = new Car("byd")
-// console.log(mycar.details())
+// // function pi(radius){
+// //     return Math.PI * radius *radius  //abstraction 
+// // // }
+// // // console.log(area(5))
 
-// function counter(){
-// var count = 0 
-//  return{
-//  inc:function(){
-//     count++
-//     console.log("count",count)
-// }
-// }
-// }
-// const c1 = counter()
-// c1.inc()
+// // // class Car{
+// //     constructor(model){
+// //         this.model=model
+// //     }
+// // details(){
+// //     console.log("car model is " + this.model)
+// //     return this.model
+// // }
+// // }
+// // const mycar = new Car("byd")
+// // console.log(mycar.details())
+
+// // function counter(){
+// // var count = 0 
+// //  return{
+// //  inc:function(){
+// //     count++
+// //     console.log("count",count)
+// // }
+// // }
+// // }
+// // const c1 = counter()
 // // c1.inc()
+// // // c1.inc()
 
+// //  -- Inheritence
+// // parent class ->superclass or base classs
+// // child class ->subclass or derived class
 
-class Zaid{
-    constructor(){}
-}
+// // const obj = {
+// //   name: "ali",
+// //   age: 20,
+// // };
+// // Object.prototype.details = function main() {
+// // console.log(`my name is ${this.name}`)
+// // };
+// // obj.details()
 
-const abc = ("zaid", "zaid")
+// // function studentData(name){
+// // this.name = name
+// // }
+// // studentData.prototype.details= function(name){
+// //     console.log(`student name is ${this.name}`)
+// // }
+// // studentData.details("ali")
+// // const mydata =new studentData("ali")
+// // mydata.details()
 
+// // es6 class
+// class ParentClass {
+//   constructor(name) {
+//     this.name = name;
+//   }
+//   details() {
+//     console.log(`parent class name is ${this.name}`);
+//   }
+// }
+// class ChildClass extends ParentClass {
+//   constructor(name) {
+//     super(name);
+//   }
+// }
+// const o = new ChildClass("bilal");
+// o.details();
 
-///  
+// // factory function
+// // return new object
+// function MainFunction(name, age) {
+//   return {
+//     name: name,
+//     age: age,
+//     details: function () {
+//       console.log(`name is ${this.name} and age is ${this.age}`);
+//     },
+//   };
+// }
+// const function1 = new MainFunction("ali", 30);
+// const function2 = new MainFunction("bilal", 20);
+// function1.details();
+// function2.details();
+
+// //  -- Encapsulation
+// // 1.closures
+// // use private variables -> for security
+// class MoneyChecker {
+//   #money;
+//   constructor(cardNum, cardMoney) {
+//     ((this.cardNum = cardNum), (this.#money = cardMoney));
+//   }
+//   sendMoney(cardMoney) {
+//     this.#money += cardMoney;
+//   }
+//   showMoney() {
+//     console.log(`available money is ${this.#money}`);
+//   }
+// }
+// const moneyTransfer = new MoneyChecker(12345, 5000);
+// moneyTransfer.sendMoney(300);
+// moneyTransfer.showMoney();
+// console.log(MoneyChecker.showMoney); //undefined due to security encapsulate
+
+// //  -- Polymorphism
+// // poly means many
+// // morphism (forms or methods)
+// // manyforms opr many methods
+
+// // parent(details(im main))
+// // chils(details(im child))
+
+// // 1.overriding
+// class MainClass {
+//     details(){
+//         console.log("im in main class")
+//     }
+// }
+// class SubClass1 extends MainClass{
+//     details(){
+//         console.log("im in sub class")
+//     }
+// }
+// class SubClass2 extends MainClass{
+//     details(){
+//         console.log("im in sub class2")
+//     }
+// }
+// const obj1 = new SubClass1()
+// const obj2 = new SubClass2()
+// const obj3 = new MainClass()
+// obj1.details()
+// obj2.details()
+// obj3.details()
+
+// // 2.overloading (compile time)
+
+// class Sum{
+//     add(num1,num2){
+//         if(num2 === undefined){
+//             return num1 + num1
+//         }
+//          return num1 + num2
+//     }
+// }
+// const addNum = new Sum()
+// console.log(addNum.add(2,3))   
